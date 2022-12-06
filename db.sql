244da73b4ed38e98d572398515159ef8c0da9f52
Sudharshan <sudharshanrms2004@gmail.com>
2022-12-06 22:21:31 +0530

    Added db tables

--- db.sql
+++ db.sql
@@ -7,3 +7,33 @@ create table admin(
     created_at datetime default current_timestamp,
     modified_at timestamp default current_timestamp
 );
+
+create table organization(
+    id int primary key not null auto_increment,
+    name varchar(256) not null,
+    logo varchar(256) not null
+);
+
+create table competition(
+    id int primary key not null auto_increment,
+    organization int not null,
+    competition varchar(256) not null,
+    image varchar(256),
+    year varchar(15) not null,
+    created_at datetime default current_timestamp,
+    modified_at timestamp default current_timestamp
+);
+
+create table participants(
+    id int primary key not null auto_increment,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar(256) not null,
+    degree varchar(15) not null,
+    organization int not null,
+    competition int not null,
+    winner int not null default 0,
+    place int,
+    created_at datetime default current_timestamp,
+    modified_at timestamp default current_timestamp
+);
