04aa611ee3fed3024b43c804792b3d54b31ea50a
Sudharshan <sudharshanrms2004@gmail.com>
2022-12-11 15:18:42 +0530

    Updated rows when already exists

--- src/models/Certificate.php
+++ src/models/Certificate.php
@@ -26,4 +26,28 @@ class Certificate extends Common{
             return ["result" => false, "message" => $e->getMessage()];
         }
     }
+
+    public function update($user_id, $certificate){
+        try{
+            $query = "UPDATE $this->table SET certificate=:certificate WHERE user_id=:user_id";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(":user_id", $user_id);
+            $stmt->bindParam(":certificate", $certificate);
+            
+            $stmt->execute();
+
+            return ["result" => true];
+        }catch(PDOException $e){
+            return ["result" => false, "message" => $e->getMessage()];
+        }
+    }
+
+    public function getFromUserID($user_id){
+        $query = "SELECT * FROM $this->table WHERE user_id=:user_id";
+        $result = $this->db->prepare($query);
+        $result->bindParam(":user_id", $user_id);
+        $result->execute();
+        $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    }
 }
--- src/models/Participants.php
+++ src/models/Participants.php
@@ -31,4 +31,25 @@ class Participants extends Common{
             return ["result" => false, "message" => $e->getMessage()];
         }
     }
+
+    public function update($first_name, $last_name, $email, $degree, $organization, $competition, $winner, $place=0){
+        try{
+            $query = "UPDATE $this->table SET first_name=:first_name, last_name=:last_name, degree=:degree, organization=:organization, competition=:competition, winner=:winner, place=:place WHERE email=:email";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(":first_name", $first_name);
+            $stmt->bindParam(":last_name", $last_name);
+            $stmt->bindParam(":email", $email);
+            $stmt->bindParam(":degree", $degree);
+            $stmt->bindParam(":organization", $organization);
+            $stmt->bindParam(":competition", $competition);
+            $stmt->bindParam(":winner", $winner);
+            $stmt->bindParam(":place", $place);
+
+            $stmt->execute();
+
+            return ["result" => true];
+        }catch(PDOException $e){
+            return ["result" => false, "message" => $e->getMessage()];
+        }
+    }
 }
