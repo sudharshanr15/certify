f2c56b0874bf57a98169adf0d703a9656d711b59
Sudharshan <sudharshanrms2004@gmail.com>
2022-12-30 18:11:33 +0530

    Added college name on mail

--- src/templates/admin/forgot_password.php
+++ src/templates/admin/forgot_password.php
@@ -66,13 +66,13 @@ function sendResetLink($email, $reset_link){
                         </tr>
                         <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                             <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                                <b>TamizhiStore</b>
+                                <b>APSCE College</b>
                                 <p>Administrator</p>
                             </td>
                         </tr>
                         <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                             <td class="content-block" style="text-align: center;font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0;" valign="top">
-                                © 2021 TamizhiStore
+                                © 2023 APSCE College
                             </td>
                         </tr>
                     </tbody></table>
