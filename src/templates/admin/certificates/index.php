84c5faa5fd50ddf16048174a4ea0ae09728c097a
Sudharshan <sudharshanrms2004@gmail.com>
2023-01-02 17:34:24 +0530

    Added users name and email on certificate list

--- src/templates/admin/certificates/index.php
+++ src/templates/admin/certificates/index.php
@@ -1,6 +1,9 @@
 <?php
 
 use Certify\Certify\models\Certificate;
+use Certify\Certify\models\Participants;
+
+$participant = new Participants;
 
 $certificate = new Certificate;
 $certificate = $certificate->getAll();
@@ -8,7 +11,7 @@ $certificate = $certificate->getAll();
 ?>
 
 <div class="col-12">
-    <h4 class="mb-2">Organization</h4>
+    <h4 class="mb-2">Certificates</h4>
     <div class="card border-0">
         <div class="card-body">
             <div class="table-responsive">
@@ -17,16 +20,23 @@ $certificate = $certificate->getAll();
                         <tr>
                             <th scope="col">id</th>
                             <th scope="col">User ID</th>
-                            <th scope="col">Image</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Certificate</th>
                         </tr>
                     </thead>
                     <tbody>
                     <?php
                         foreach($certificate as $c){
+                            $user = $participant->get($c['user_id']);
                             ?>
                             <tr>
                                 <th scope="row"><?= $c['id'] ?></th>
                                 <td><?= $c['user_id'] ?></td>
+                                <td><?= $user['first_name'] ?></td>
+                                <td><?= $user['last_name'] ?></td>
+                                <td><?= $user['email'] ?></td>
                                 <td><img src="<?= $c['certificate'] ?>" /></td>
                             </tr>
                             <?php
